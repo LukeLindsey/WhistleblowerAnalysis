ba2476f7b47e88b3b86c8f23b72cfc2d05678a07
Luke Lindsey <jll0026@tigermail.auburn.edu>
2015-04-06 00:37:29 -0500

    Added type checking for G+ post

--- SocialNetworkSearch/GooglePlus/GooglePost.py
+++ SocialNetworkSearch/GooglePlus/GooglePost.py
@@ -9,12 +9,19 @@ the Twitter API.
 
 class GooglePost(object):
 
-	def __init__(self, result):
-		actor = result["actor"]
-		self.author = actor["displayName"]
-		my_object = result["object"]
-		self.post = my_object["content"]
-		self.post = self.post.replace('\\ufeff', '')  # remove endian order code
-		self.date = result["updated"]
+	def __init__(self, result=None):
+		if result is None:
+			raise TypeError('\'result\' is required for GooglePost.')
+		elif not isinstance(result, dict):
+			raise TypeError('\'result\' passed in must be a dict.')
+		try:
+			actor = result["actor"]
+			self.author = actor["displayName"]
+			my_object = result["object"]
+			self.post = my_object["content"]
+			self.post = self.post.replace('\\ufeff', '')  # remove endian order code
+			self.date = result["updated"]
+		except KeyError:
+			raise TypeError('\'result\' is not a valid result from G+. Causes KeyError.')
 
 # might add location and/or url
